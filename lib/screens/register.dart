8c4aab80018567be88a4112e25597c174e2bd82b
Gareth Harris <gdharris1000@gmail.com>
2020-05-29 09:12:41 +0100

    added users database table

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = Firestore.instance;
 
 FirebaseUser loggedInUser;
 
@@ -10,8 +13,30 @@ class RegisterScreen extends StatefulWidget {
 
 class _RegisterScreenState extends State<RegisterScreen> {
   final _auth = FirebaseAuth.instance;
+  String name;
   String email;
   String password;
+
+  void addToDatabase() async {
+    try {
+      final user = await _auth.currentUser();
+      if (user != null) {
+        loggedInUser = user;
+
+        try {
+          final newUser = await _firestore.collection('users').add({
+            'uid': loggedInUser.uid,
+            'name': name,
+            'created': Timestamp.now()
+          });
+        } catch (e) {
+          print(e);
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,6 +44,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
         body: SafeArea(
       child: Column(
         children: <Widget>[
+          TextField(
+            decoration: InputDecoration(hintText: "Name"),
+            onChanged: (value) {
+              name = value;
+            },
+          ),
           TextField(
             decoration: InputDecoration(hintText: "Email address"),
             onChanged: (value) {
@@ -34,8 +65,12 @@ class _RegisterScreenState extends State<RegisterScreen> {
           RaisedButton(
             onPressed: () async {
               try {
-                await _auth.createUserWithEmailAndPassword(
+                final newUser = await _auth.createUserWithEmailAndPassword(
                     email: email, password: password);
+
+                if (newUser != null) {
+                  addToDatabase();
+                }
               } catch (e) {
                 print(e);
               }
