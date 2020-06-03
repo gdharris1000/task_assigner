3420c8fa27d75763146ae130630562030bf6afc9
Gareth Harris <gdharris1000@gmail.com>
2020-06-03 15:12:11 +0100

    shows the name of the user who created the task in the task details

--- lib/widgets/task_details.dart
+++ lib/widgets/task_details.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:DoMyBidding/models/task.dart';
 import 'package:intl/intl.dart';
+import 'package:DoMyBidding/models/user_data.dart';
 
-class TaskDetails extends StatelessWidget {
+class TaskDetails extends StatefulWidget {
   final Task task;
 
   TaskDetails(this.task);
 
+  @override
+  _TaskDetailsState createState() => _TaskDetailsState();
+}
+
+class _TaskDetailsState extends State<TaskDetails> {
+  String userName = "";
+
   Text priority() {
-    if (task.priority == 1) {
+    if (widget.task.priority == 1) {
       return Text('High');
-    } else if (task.priority == 2) {
+    } else if (widget.task.priority == 2) {
       return Text("Medium");
-    } else if (task.priority == 3) {
+    } else if (widget.task.priority == 3) {
       return Text("Low");
+    } else {
+      return Text('High');
     }
+  }
+
+  void getUserName() {
+    GetUserInfo().getUserName(widget.task.createdBy).then((String result) {
+      setState(() {
+        userName = result;
+      });
+    });
   }
 
   String dateToString(date) {
@@ -24,9 +42,15 @@ class TaskDetails extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    getUserName();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return AlertDialog(
-      title: Text(task.task),
+      title: Text(widget.task.task),
       content: Column(
         children: <Widget>[
           Row(
@@ -40,14 +64,21 @@ class TaskDetails extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text('Due Date: '),
-              Text(dateToString(task.dueDate)),
+              Text(dateToString(widget.task.dueDate)),
             ],
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text('Date Created: '),
-              Text(dateToString(task.created)),
+              Text(dateToString(widget.task.created)),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Created by: '),
+              Text(userName),
             ],
           ),
         ],
