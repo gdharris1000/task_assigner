46a537acc4c02a7406d2e7f8bda6aacb903487d9
Gareth Harris <gdharris1000@gmail.com>
2020-06-03 15:01:56 +0100

    added priority and due date to task details

--- lib/widgets/task_details.dart
+++ lib/widgets/task_details.dart
@@ -1,17 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:DoMyBidding/models/task.dart';
+import 'package:intl/intl.dart';
 
 class TaskDetails extends StatelessWidget {
   final Task task;
 
   TaskDetails(this.task);
 
+  Text priority() {
+    if (task.priority == 1) {
+      return Text('High');
+    } else if (task.priority == 2) {
+      return Text("Medium");
+    } else if (task.priority == 3) {
+      return Text("Low");
+    }
+  }
+
+  String dateToString(date) {
+    DateTime timestampToDate = date.toDate();
+    String formattedDate = DateFormat('dd-MM-yyyy').format(timestampToDate);
+    return formattedDate;
+  }
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text(task.task),
       content: Column(
-        children: <Widget>[Text(task.priority.toString())],
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Priority: '),
+              priority(),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Due Date: '),
+              Text(dateToString(task.dueDate)),
+            ],
+          ),
+        ],
       ),
     );
   }
