16cb8dde428f92e8a0264ba9fa34eb80ed04edb5
Gareth Harris <gdharris1000@gmail.com>
2020-06-17 09:02:47 +0100

    added form widget and validation to login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,3 @@
-import 'package:TaskAssigner/screens/view_tasks.dart';
 import 'package:TaskAssigner/screens/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -13,6 +12,7 @@ class _LoginScreenState extends State<LoginScreen> {
   String email;
   String password;
   final _auth = FirebaseAuth.instance;
+  final formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -33,35 +33,53 @@ class _LoginScreenState extends State<LoginScreen> {
                 flex: 2,
               ),
               SizedBox(height: 30.0),
-              Flexible(
-                flex: 5,
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(hintText: 'Email address'),
-                      onChanged: (value) => email = value,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(hintText: 'Password'),
-                      obscureText: true,
-                      onChanged: (value) => password = value,
-                    ),
-                    RaisedButton(
-                      onPressed: () async {
-                        try {
-                          final user = await _auth.signInWithEmailAndPassword(
-                              email: email, password: password);
-                          if (user != null) {
-//                        Navigator.pushNamed(context, TasksScreen.id);
-                            Navigator.pushNamed(context, HomeScreen.id);
+              Form(
+                key: formKey,
+                child: Flexible(
+                  flex: 5,
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(hintText: 'E-mail address'),
+                        onChanged: (value) => email = value,
+                        validator: (value) {
+                          if (!value.contains('@')) {
+                            return 'Invalid e-mail address';
                           }
-                        } catch (e) {
-                          print(e);
-                        }
-                      },
-                      child: Text('Log in'),
-                    )
-                  ],
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(hintText: 'Password'),
+                        obscureText: true,
+                        onChanged: (value) => password = value,
+                        validator: (value) {
+                          if (value.length < 4) {
+                            return 'Password is too short';
+                          }
+                          return null;
+                        },
+                      ),
+                      RaisedButton(
+                        onPressed: () async {
+                          if (formKey.currentState.validate()) {
+                            try {
+                              final user =
+                                  await _auth.signInWithEmailAndPassword(
+                                      email: email, password: password);
+                              if (user != null) {
+//                        Navigator.pushNamed(context, TasksScreen.id);
+                                Navigator.pushNamed(context, HomeScreen.id);
+                              }
+                            } catch (e) {
+                              print(e);
+                            }
+                          }
+                        },
+                        child: Text('Log in'),
+                      )
+                    ],
+                  ),
                 ),
               ),
             ],
